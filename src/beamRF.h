2aebdcbf68c76c2f56a6b66aea6ccb10ce5b860e
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2020-01-18 10:50:27 -0600

    Double loop on file reading was causing unpredictable behaviour.

--- src/beamRF.h
+++ src/beamRF.h
@@ -153,7 +153,7 @@ public:
 			first=0;
 		if(limit>n || limit==0) // 0 means no limit
 			limit=n;
-		for(int i=0,j=0;i<limit;j++,++i,i%=n)
+		for(int j=0; j < limit; j++)
 		{   
 			RootFReader<ND5Event> reader( names[j], "h3002");
 			for(int k=0;k<reader.Count();k++)
